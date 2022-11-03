0c4301a3e7b87f4680279da81d04d554797649b2
Rawle Becerril <rawlesxm@gmail.com>
2022-11-02 22:54:38 -0400

    magic 8 ball h1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/magic/:question', (req, res) => {
     const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
-    res.send(`${req.params.question}? Answer: ${answers[Math.floor(Math.random()*answers.length)]}`)
+    res.send(`${req.params.question}? <h1>${answers[Math.floor(Math.random()*answers.length)]}</h1>`)
 })
 
 
