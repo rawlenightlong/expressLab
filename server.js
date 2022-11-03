e4a42edc8d452a6ba1ce84db19923de9a373d6ab
Rawle Becerril <rawlesxm@gmail.com>
2022-11-02 22:52:57 -0400

    magic 8 ball

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // SETUP - DECLARE DEPENDENCIES - CREATE APP OBJECT
 //////////////////////////////////
 
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 3000
@@ -19,7 +20,12 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
     response.send("Your tip is $" + (request.params.total * (request.params.tipPercentage / 100)))
 })
 
-app.get('/magic')
+app.get('/magic/:question', (req, res) => {
+    const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+    res.send(`${req.params.question}? Answer: ${answers[Math.floor(Math.random()*answers.length)]}`)
+})
 
 
 
