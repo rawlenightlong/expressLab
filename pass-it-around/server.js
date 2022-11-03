ca5ec6f0895495362d08d696637dcf3a9b40450a
Rawle Becerril <rawlesxm@gmail.com>
2022-11-02 23:55:08 -0400

    if statements for 1 bottle

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -2,7 +2,7 @@
 // SETUP - DECLARE DEPENDENCIES - CREATE APP OBJECT
 //////////////////////////////////
 
-const { response } = require('express')
+
 const express = require('express')
 const app = express()
 const PORT = 3000
@@ -15,13 +15,16 @@ app.get('/', (req, res) => {
     res.send(`99 bottles of beer on the wall, 99 bottles of beeeeer, <a href="http://localhost:3000/98">Take One Down, Pass It Around</a>`)
 })
 
+app.get('/empty', (req, res) => {
+    res.send(`No bottles of beer on the wall! <a href="http://localhost:3000/99">Start Over<a>`)
+})
+
 app.get('/:bottlesOfBeer', (req, res) => {
-    if (req.params.bottlesOfBeer > 0){
+    if (req.params.bottlesOfBeer > 1){
         res.send(` ${req.params.bottlesOfBeer} bottles of beer on the wall! ${req.params.bottlesOfBeer} bottles of beer on the wall, ${req.params.bottlesOfBeer} bottles of beeeeeer, <a href="http://localhost:3000/${req.params.bottlesOfBeer - 1}">Take One Down, Pass It Around</a>`)
-    }else {
-        res.send(`No more bottles of beer on the wall! <a href="http://localhost:3000/99"> Start Over</a>`)
+    }if (req.params.bottlesOfBeer = 1){
+        res.send(`${req.params.bottlesOfBeer} bottle of beer on the wall! ${req.params.bottlesOfBeer} bottle of beeeeeeer, <a href="http://localhost:3000/empty"> Take One Down, Pass It Around</a>`)
     }
-    
 })
 
 
